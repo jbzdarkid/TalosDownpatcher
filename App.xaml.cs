b3436e36e4bf44d9d24513f21b2fbbda867a64c6
jbzdarkid <jbzdarkid@gmail.com>
2019-06-02 17:00:20 -0700

    oops

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows;
 namespace TalosDownpatcher {
   public partial class App : Application {
     private void LaunchOldVersion(object sender, StartupEventArgs e) {
-      if (e.Args.Length > 0 && e.Args[1] == "LaunchOldVersion") {
+      if (e.Args.Length > 0 && e.Args[0] == "LaunchOldVersion") {
         DateUtils.SetYears(-3);
         SteamCommand.StartGame();
         DateUtils.SetYears(+3);
