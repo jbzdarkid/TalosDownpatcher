2e59a798b287ff6d99c16ba5d60cc6e0a1db67e3
jbzdarkid <jbzdarkid@gmail.com>
2019-12-18 06:42:06 -0800

    rename

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows;
 
 namespace TalosDownpatcher {
   public partial class App : Application {
-    private void LaunchOldVersion(object sender, StartupEventArgs e) {
+    private void AppStartup(object sender, StartupEventArgs e) {
       if (e.Args.Length > 0 && e.Args[0] == "LaunchOldVersion") {
         int yearDelta = DateUtils.GetCurrentYear() - 2016;
         Logging.Log($"Changing date in order to launch old version. YearDelta: {yearDelta}");
